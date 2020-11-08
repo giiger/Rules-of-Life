20ed951402fa7f073e4df5ea6c845e4c94507743
Alex Giger <57809929+giiger@users.noreply.github.com>
2020-11-08 09:48:38 -0800

    Initial Commit

---
+++ Rules of Life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rules of Life
+//
+//  Created by Alexander Giger on 11/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
